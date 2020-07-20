1aac4b454345a429a7e429048c3b12091da4fe72
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-19 20:30:43 -0700

    react

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -11,7 +11,9 @@ const Accordion = ({ items }) => {
       <React.Fragment key={items.title}>
         <div
           className="title active"
-          onClick={() => {onTitleClick}}
+          // must wrap onTitleClick(index) with arrow function othersise
+          // it is invoked on rendering
+          onClick={() => {onTitleClick(index)}}
         >
           <i className="dropdown icon"></i>
           {item.title}
