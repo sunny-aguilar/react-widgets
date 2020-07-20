a22872fe9531f2a60851f6e197df78e5b6ba4341
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-19 21:46:15 -0700

    react

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -13,7 +13,7 @@ const Accordion = ({ items }) => {
   // const things = useState(null);
   // const activeIndex = things[0];
   // const seActiveIndex = things[1];
- /
+ //
   // helper function inside functional component
   const onTitleClick = index => {
     setActiveIndex(index);
