82de4b6a2d5d472df5b0ee857f04506e24a629ac
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-19 18:23:31 -0700

    react

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -1,6 +1,16 @@
 import React from "react";
 
-const Accordion = ({ items }) => {
+const Accordion = ({items}) => {
+  const renderedItems = items.map(item => {
+    return (
+      <div>
+        <div className="title active">
+          <i className="dropdown icion"></i>
+          {item.title}
+        </div>
+      </div>
+    );
+  });
   return <div>{items.length}</div>;
 };
 
