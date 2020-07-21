717b5b27d9c0bea115703468a8cb1e907b6b5f4c
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-20 22:25:56 -0700

    react

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Accordion from "./components/Accordion.js";
+import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 
 const items = [
