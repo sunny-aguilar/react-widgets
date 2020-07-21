d9d35bbdd72921477de482d8b3298873a3fd5971
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-20 22:43:36 -0700

    react

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from "react";
 const Search = () => {
   const [term, setTerm] = useState("");
 
+  useEffect(() => {
+    console.log("DD");
+  });
+
   return (
     <div>
       <div className="ui form">
@@ -12,7 +16,7 @@ const Search = () => {
             className="input"
             type="text"
             value={term}
-            onChange={e => setTerm(e.target.value)}
+            onChange={(e) => setTerm(e.target.value)}
           />
         </div>
       </div>
