1d1cda67296e91149aa4814fdc70cddfadefee14
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-23 16:33:49 -0700

    react

--- src/components/Search.js
+++ src/components/Search.js
@@ -22,7 +22,7 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    setTimeout( () => {
+    const timeoutId = setTimeout( () => {
       if (term) {
         search();
       }
