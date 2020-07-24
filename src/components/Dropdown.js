63306647218010b6e51b088bef0060fcd45195cd
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-24 15:21:54 -0700

    react

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -13,6 +13,10 @@ const Dropdown = ({ options }) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon"></i>
+          
+        </div>
       </div>
     </div>
   );
