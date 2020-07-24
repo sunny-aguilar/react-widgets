2b27aca1ba3e7c8427ac5462e10535177df635bb
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-24 06:57:01 -0700

    react

--- src/components/Search.js
+++ src/components/Search.js
@@ -38,25 +38,28 @@ const Search = () => {
   useEffect(() => {
     if (term && !results.length) {
       search();
-    }
-    else {
+    } else {
       const timeoutId = setTimeout(() => {
         if (term) {
           search();
         }
-      },800);
+      }, 800);
       return () => {
         clearTimeout(timeoutId);
       };
     }
-
   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
         <div className="right floated content">
-          <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
         </div>
         <div className="content">
           <div className="header">{result.title}</div>
