5e60e49e57a7531b968dda099fdacf583f8c1b53
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-24 11:06:04 -0700

    react

--- src/app.js
+++ src/app.js
@@ -18,6 +18,21 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+];
+
 export default () => {
   return (
     <div>
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,20 +1,5 @@
 import React from "react";
 
-const options = [
-  {
-    label: "The Color Red",
-    value: "red",
-  },
-  {
-    label: "The Color Green",
-    value: "green",
-  },
-  {
-    label: "The Color Blue",
-    value: "blue",
-  },
-];
-
 const Dropdown = () => {
   return <h1>Dropdown</h1>;
 };
