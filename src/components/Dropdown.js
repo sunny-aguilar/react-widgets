df953febc35189ce1eb464d7234281e5efa5328c
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-24 11:05:23 -0700

    react

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const options = [
-  {},{},{}
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
 ];
 
 const Dropdown = () => {
