2293fa2d2fd3b4cb9dd60a61ac715fd2a15aac21
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-25 13:09:26 -0700

    react

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Dropdown = ({ options }) => {
-  const renderedOptions = options.map((options) => {
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  // state
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.addEventListener('click', () => {
+      console.log("Body clicked")
+      setOpen(false);
+    });
+  });
+
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
     return (
-      <div className="item" key={options.value}>
-        {options.label}
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => onSelectedChange(option)}
+      >
+        {option.label}
       </div>
     );
   });
@@ -13,10 +30,13 @@ const Dropdown = ({ options }) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? 'visible active' : ""} `}
+        >
           <i className="dropdown icon"></i>
-          <div className="text">Select Color</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>{renderedOptions}</div>
         </div>
       </div>
     </div>
