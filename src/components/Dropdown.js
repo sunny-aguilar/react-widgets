672e9da7de019ef1e92195144958a5bbbb156fa0
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-25 13:55:34 -0700

    react

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -29,6 +29,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     );
   });
 
+  console.log(ref.current);
   return (
     <div ref={ref} className="ui form">
       <div className="field">
