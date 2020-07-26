229140bc14798ae59ad9061e6d3849c60ea7ce38
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-25 22:27:58 -0700

    react

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,7 +2,7 @@
 // useState, useEffect, useRef
 import React, { useState, useEffect, useRef } from "react";
 
-// s
+// se
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   // state
   const [open, setOpen] = useState(false);
