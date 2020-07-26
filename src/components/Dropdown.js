cc7bbeec3525c9669ab4045c06e419314a1d9f42
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-26 12:52:27 -0700

    react

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -9,7 +9,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   const ref = useRef();
 
   useEffect(() => {
-    document.body.addEventListener('click', (event) => {
+    document.body.addEventListener("click", (event) => {
       if (ref.current.contains(event.target)) {
         return;
       }
@@ -41,11 +41,13 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         <label className="label">Select a Color</label>
         <div
           onClick={() => setOpen(!open)}
-          className={`ui selection dropdown ${open ? 'visible active' : ""} `}
+          className={`ui selection dropdown ${open ? "visible active" : ""} `}
         >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className={`menu ${open ? "visible transition" : ""}`}>{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
