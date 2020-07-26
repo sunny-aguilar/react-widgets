7331b34dfae4622c17704f202f3122d997ab9c9c
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-25 22:29:54 -0700

    react

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 
-// ac
+// acc
 const items = [
   {
     title: "What is React?",
