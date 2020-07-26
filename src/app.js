e4e609f1f6b079071ba3070d7d9aa5f5806fab68
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-25 22:30:50 -0700

    react

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 
-// accordion state par
+// accordion state para
 const items = [
   {
     title: "What is React?",
