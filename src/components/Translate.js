1c5398e1f8132047b6e9a705113bbdf8a5de9e14
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-27 16:49:02 -0700

    react

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
 
+// options
+const options = [
+  {
+    label: "Afrikaans",
+    value: "af"
+  },
+  {
+    label: "Arabic",
+    value: "ar"
+  }
+];
+
 const Translate = () => {
   return <div><Dropdown /></div>;
 };
