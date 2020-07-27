d0e2c711f30472c1c0861664ea6836ee1bb3a244
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-27 11:36:07 -0700

    Updates

---
+++ precache-manifest.a964d4515379f7121cb4b632f6d4c5d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f678f306a5f4e2553366b2ce8c6f2f2f",
+    "url": "/react-widgets/index.html"
+  },
+  {
+    "revision": "1152e8ad505e3023397d",
+    "url": "/react-widgets/static/js/2.f05bf877.chunk.js"
+  },
+  {
+    "revision": "e46fc7d39878ee64e7b083a156855138",
+    "url": "/react-widgets/static/js/2.f05bf877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a804c002c88cc5c11d34",
+    "url": "/react-widgets/static/js/main.26272130.chunk.js"
+  },
+  {
+    "revision": "af9080dd027d4969290e",
+    "url": "/react-widgets/static/js/runtime-main.271165c5.js"
+  }
+]);
