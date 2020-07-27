e3a3a8db1689e28baa5bf89cf222d842ddf20c12
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-27 11:29:45 -0700

    react

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 
 // render app
+// App is being developed in a separte module to keep index.js clean
 ReactDOM.render(<App />, document.querySelector("#root"));
